9dcab8707184b831edce68cb403e6a813a2be08b
migaa10 <migaraten@gmail.com>
2022-11-02 23:06:53 +0530

    add update many function in orders

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,14 +1,33 @@
 const itemsModal = require('../models/itemModel');
 
-module.exports.saveItem = async (req,res) => {
+module.exports.saveItem = async (req, res) => {
     let newItem = itemsModal(req.body);
     console.log(newItem);
-    try{
+    try {
         const savedItem = await itemsModal.create(newItem);
-        res.status(200).json({state:true, msg: "new item saved successfully!"})
-    }catch(error) {
+        res.status(200).json({ state: true, msg: "new item saved successfully!" })
+    } catch (error) {
         console.log(error)
-        res.status(400).json({state:false, msg: "new item saved unsuccessfully!"})
+        res.status(400).json({ state: false, msg: "new item saved unsuccessfully!" })
     }
 }
 
+module.exports.updateProduct = async (req, res) => {
+    try {
+        const writeOperations = req.body.map((x) => {
+            return {
+                updateOne: {
+                    filter: { item_id: x.data.item_id },
+                    update: { item_qty: (x.data.item_qty - x.qty) }
+                }
+            };
+        });
+
+        await itemsModal.bulkWrite(writeOperations);
+
+        res.status(200).json({ state: true, msg: 'checkout successfully' })
+    } catch (e) {
+        res.status(400).json({ state: false, msg: 'checkout unsuccessfully' })
+    }
+
+}
