c2f73b7bc2b075bd2821844894510917077ca46c
migaa10 <migaraten@gmail.com>
2022-11-23 14:13:55 +0530

    image upload

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const path = require('path')
+
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({
+    storage: storage
+})
 
 
-router.post('/', productController.saveProduct); // create new product
+router.post('/', upload.single('file'), productController.saveProduct); // create new product
 router.get('/', productController.getProductById); // get product
 
 
