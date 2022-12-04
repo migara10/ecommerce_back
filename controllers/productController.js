d998d3cd867f1dc067d49d70e0d7f15aada88342
migaa10 <migaraten@gmail.com>
2022-12-05 00:51:50 +0530

    add product edit api

--- controllers/productController.js
+++ controllers/productController.js
@@ -18,6 +18,26 @@ module.exports.saveProduct = async (req, res) => {
         res.status(400).json({ state: false, msg: "new product saved unsuccessfully!" })
     }
 }
+module.exports.editProduct = async (req, res) => {
+    let newProduct = productModel({
+        product_name: req.body.product_name,
+        product_category: req.body.product_category,
+        product_name: req.body.product_disc,
+        product_disc: req.body.product_name,
+        product_price: req.body.product_price,
+        product_imageuri: "",
+        // product_imageuri: req.file.path.replace(/\\/g, "/"),
+    });
+    const query = { product_id: parseInt(req.body.product_id) }
+    try {
+        const savedProduct = await productModel.updateOne(query, req.body);
+        res.status(200).json({ state: true, msg: "new product saved successfully!", data: savedProduct })
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ state: false, msg: "new product saved unsuccessfully!" })
+    }
+}
 
 module.exports.getProducts = async (req, res) => {
     /* const allProducts = await productModel.find({});
@@ -46,16 +66,16 @@ module.exports.getProducts = async (req, res) => {
         });
 }
 module.exports.getProductById = async (req, res) => {
-   var pipeline = [
-    { $match: { product_id: parseInt(req.params.id) } },
-]
-productModel.aggregate(pipeline)
-    .then((product) => {
-        res.status(200).json({ state: true, data: product })
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+    var pipeline = [
+        { $match: { product_id: parseInt(req.params.id) } },
+    ]
+    productModel.aggregate(pipeline)
+        .then((product) => {
+            res.status(200).json({ state: true, data: product })
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 
